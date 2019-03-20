c4ff583791aa5bb6d02bdedeefed731e880dbcb9
Matt Edmonston <matt@ethercreative.co.uk>
2019-03-20 10:26:39 +0000

    Setting map to false skips eager load.

--- src/traits/GatherWith.php
+++ src/traits/GatherWith.php
@@ -18,11 +18,11 @@ trait GatherWith
     {
         $with = [];
 
-        if (!$type && $this->hasProperty('type') && $this->type)
+        if (!$type && $this->hasProperty('type') && $this->type) {
             $type = $this->type;
+        }
 
-        if ($type)
-        {
+        if ($type) {
             $with = $type::$with;
             $withMap = $this->withMap ?: $type::$withMap;
         }
@@ -34,21 +34,24 @@ trait GatherWith
 
         // die('<pre>'.print_r([ 'with' => $with, 'alias' => $this->_filterWithAliased, ],1).'</pre>');
 
-        if (!empty($with))
-        {
-            if (!empty($withMap))
-            {
-                foreach ($with as &$relation)
-                {
-                    if (array_key_exists($relation, $withMap))
+        if (!empty($with)) {
+            if (!empty($withMap)) {
+                foreach ($with as &$relation) {
+                    if (array_key_exists($relation, $withMap)) {
                         $relation = $withMap[$relation];
+                    }
 
-                    if (is_array($relation))
-                    {
+                    $firstPart = explode('.', $relation)[0];
+
+                    if (ArrayHelper::getValue($withMap, $firstPart) === false) {
+                        $relation = null;
+                        continue;
+                    }
+
+                    if (is_array($relation)) {
                         $first = array_shift($relation);
 
-                        foreach ($relation as $r)
-                        {
+                        foreach ($relation as $r) {
                             $with[] = $r;
                         }
 
@@ -69,18 +72,20 @@ trait GatherWith
 
     protected function gatherWith($selectedFields, &$with, $parent = null)
     {
-        if ($parent && !in_array($parent, $with))
+        if ($parent && !in_array($parent, $with)) {
             $with[] = $parent;
+        }
 
-        foreach ($selectedFields as $field => $value)
-        {
+        foreach ($selectedFields as $field => $value) {
             $key = !in_array($field, ['edges', 'node', 'pageInfo', 'totalCount']) ? $field : null;
 
-            if (is_array($value))
+            if (is_array($value)) {
                 $this->gatherWith($value, $with, join('.', array_filter([$parent, $key])));
+            }
 
-            if ($key)
+            if ($key) {
                 $with[] = join('.', array_filter([$parent, $field]));
+            }
         }
     }
 
@@ -88,42 +93,41 @@ trait GatherWith
     {
         // if ($parent && !in_array($with, ['id', 'title', 'status'])) die('<pre>'.print_r([ $with, $modelClass, $parent ],1).'</pre>');
 
-        if (is_string($with))
+        if (is_string($with)) {
             $with = [$with];
+        }
 
-        foreach ($with as $key => $path)
-        {
-            if (in_array($path, $this->_filterWithAliased) || !$modelClass)
+        foreach ($with as $key => $path) {
+            if (in_array($path, $this->_filterWithAliased) || !$modelClass) {
                 continue;
+            }
 
-            if (strpos($path, '.') === false)
-            {
+            if (strpos($path, '.') === false) {
                 try {
-                    if (empty($this->_filterWithCache[$modelClass]))
+                    if (empty($this->_filterWithCache[$modelClass])) {
                         $this->_filterWithCache[$modelClass] = new $modelClass;
+                    }
 
-                    if ($alias = ArrayHelper::getValue($this->_filterWithCache[$modelClass], "filterWithMap.{$path}"))
-                    {
-                        if (!is_array($alias))
+                    if ($alias = ArrayHelper::getValue($this->_filterWithCache[$modelClass], "filterWithMap.{$path}")) {
+                        if (!is_array($alias)) {
                             $alias = [$alias];
+                        }
 
-                        foreach ($alias as $additional)
-                        {
+                        foreach ($alias as $additional) {
                             $value = join('.', array_filter([$parent, $additional]));
                             $with[] = $value;
                             $this->_filterWithAliased[] = $value;
                         }
 
                         unset($with[$key]);
-                    }
-                    else
-                    {
+                    } else {
                         $isQuery = $this->_filterWithCache[$modelClass]->{'get'.$path}();
 
-                        if ($isQuery InstanceOf \yii\db\Query)
+                        if ($isQuery instanceof \yii\db\Query) {
                             $this->_filterWithMap[$path] = $isQuery->modelClass;
-                        else
+                        } else {
                             unset($with[$key]);
+                        }
                     }
                 } catch (\Exception $e) {
                     unset($with[$key]);
@@ -136,23 +140,24 @@ trait GatherWith
 
             $this->filterWith($subPath, $this->_filterWithMap[$root], join('.', array_filter([$parent, $root])));
 
-            if (is_array($subPath))
+            if (is_array($subPath)) {
                 $subPath = ArrayHelper::getValue($subPath, '0');
+            }
 
-            if (!$subPath)
+            if (!$subPath) {
                 unset($with[$key]);
+            }
         }
     }
 
     protected function gatherWithLegacy($selectedFields, &$with)
     {
-        if (!is_array($selectedFields))
+        if (!is_array($selectedFields)) {
             return;
+        }
 
-        foreach($selectedFields as $field => $children)
-        {
-            if ($field === 'edges')
-            {
+        foreach ($selectedFields as $field => $children) {
+            if ($field === 'edges') {
                 $children = ['edges' => $children];
                 // $field = $rootFieldFallback;
                 $field = null;
@@ -160,8 +165,7 @@ trait GatherWith
 
             $isEdge = !empty($children['edges']);
 
-            if ($isEdge)
-            {
+            if ($isEdge) {
                 $connectionTypeName = Inflector::camelize(Inflector::singularize($field)) . 'ConnectionType';
                 $connectionType = $this->typeNamespace . str_replace('Linked', '', $connectionTypeName);
 
@@ -170,45 +174,40 @@ trait GatherWith
 
                 $nodes = ArrayHelper::getValue($children, 'edges.node');
 
-                if (!$nodes)
+                if (!$nodes) {
                     $nodes = ArrayHelper::getValue($children, 'node');
+                }
 
-                if ($nodes)
-                {
+                if ($nodes) {
                     $sub = [];
 
-                    foreach ($nodes as $key => $value)
-                    {
-                        if (is_array($value))
+                    foreach ($nodes as $key => $value) {
+                        if (is_array($value)) {
                             $this->gatherWith([$key => $value], $sub);
+                        }
                     }
 
-                    if (!empty($sub))
-                    {
-                        foreach ($sub as $s)
+                    if (!empty($sub)) {
+                        foreach ($sub as $s) {
                             $with[] = join('.', array_filter([$field, $s]));
+                        }
                     }
                 }
-            }
-            elseif($field === 'node')
-            {
+            } elseif ($field === 'node') {
                 $sub = [];
 
-                foreach ($children as $key => $value)
-                {
-                    if (is_array($value))
+                foreach ($children as $key => $value) {
+                    if (is_array($value)) {
                         $this->gatherWith([$key => $value], $sub);
+                    }
                 }
 
-                if (!empty($sub))
-                {
-                    foreach ($sub as $s)
+                if (!empty($sub)) {
+                    foreach ($sub as $s) {
                         $with[] = $s;
+                    }
                 }
-
-            }
-            elseif(is_array($children))
-            {
+            } elseif (is_array($children)) {
                 $with[] = $field;
             }
         }
